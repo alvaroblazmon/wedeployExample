f04b0070199ab309c5790f1761411d775acf38eb
Alvaro BM <alvarob25@gmail.com>
2017-09-12 11:52:54 +0200

    Update index.js

--- ui/index.js
+++ ui/index.js
@@ -3,7 +3,18 @@ var form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
 	e.preventDefault();
 
-// Insert save data method below
-
-// Insert save data method above
+WeDeploy
+    .data('db-<projectID>.wedeploy.io')
+    .create('tasks', {name: form.item.value })
+    .then(function(response) {
+        form.reset();
+        form.item.focus();
+        console.info('Saved:', response);
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+	
 });
+
+
