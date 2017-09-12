57feee04bb58c205d825ddbaf2b85ae19f261e06
Alvaro BM <alvarob25@gmail.com>
2017-09-12 11:54:28 +0200

    Update list.js

--- ui/list.js
+++ ui/list.js
@@ -1,9 +1,16 @@
 var list = document.querySelector('.list');
 
-
-// Insert fetch data method below
-
-// Insert fetch data method above
+WeDeploy
+    .data('db-noticiasleganes.wedeploy.io')
+    .orderBy('id', 'desc')
+    .limit(5)
+    .get('tasks')
+    .then(function(response) {
+        appendTasks(response);
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
 
 function appendTasks(tasks) {
 	var taskList = '';
